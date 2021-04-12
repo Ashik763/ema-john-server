e843ec3271a3f8e7f69a10f2a924d75b4441885e
Ashik Ghosh <63238405+Ashik763@users.noreply.github.com>
2021-04-12 08:41:45 +0600

    my first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moazf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const app = express()
+const port = 5000;
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("emaJohnStore").collection("products");
+  const ordersCollection = client.db("emaJohnStore").collection("products");
+  // console.log('database connected');
+
+ app.post('/addProduct',(req, res) => {
+    
+     const products = req.body;
+     console.log(products);
+    productsCollection.insertOne(products)
+//     .toArray((err,documents) => {
+//       res.send(documents)
+//     })
+    .then(result => {
+        // console.log(result);
+        res.send(result.insertedCount);
+    })
+
+ })
+ app.get('/products', (req, res) => {
+  productsCollection.find({})
+  .toArray((err,documents) => {
+    res.send(documents);
+  })
+})
+ app.get('/product/:key', (req, res) => {
+  productsCollection.find({key: req.params.key})
+  .toArray((err,documents) => {
+    res.send(documents[0]);
+  })
+})
+
+app.post('/productsByKeys', (req, res) =>{
+  const productKeys = req.body;
+    productsCollection.find({key:{$in: productKeys}})
+  .toArray((err,documents) => {
+    res.send(documents);
+    })
+
+ })
+
+app.post('/addOrder',(req, res) => {
+    
+    const order = req.body;
+    console.log(products);
+   ordersCollection.insertOne(order)
+//     .toArray((err,documents) => {
+//       res.send(documents)
+//     })
+   .then(result => {
+       // console.log(result);
+       res.send(result.insertedCount > 0);
+   })
+
+})
+
+
+
+});
+
+
+
+
+// console.log(process.env.DB_USER);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Ema watson!');
+// })
+
+
+app.listen(port)
